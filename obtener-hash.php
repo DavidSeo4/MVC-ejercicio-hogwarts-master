18c3b702673858c051c9dbd92a45dd7b0a91f7a6
= <=>
2022-01-12 12:33:49 +0100

    ok

---
+++ obtener-hash.php
@@ -0,0 +1,9 @@
+<?php
+echo password_hash("concevigo",  PASSWORD_BCRYPT, ['cost'=>12]) ?>
+?>
+<?php
+echo password_hash("david",  PASSWORD_BCRYPT, ['cost'=>12]) ?>
+?>
+<?php
+echo password_hash("angela",  PASSWORD_BCRYPT, ['cost'=>12]) ?>
+?>
